c374a65b652dd744b3859fdc692a4f5ede0b63da
TheKelp1e <vova_krasnov_2004@mail.ru>
2023-10-27 00:51:43 +0300

    Add Swagger 2

--- src/main/java/com/example/caselabproject/CaseLabProjectApplication.java
+++ src/main/java/com/example/caselabproject/CaseLabProjectApplication.java
@@ -2,10 +2,6 @@ package com.example.caselabproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 public class CaseLabProjectApplication {
--- src/main/java/com/example/caselabproject/controllers/PublicController.java
+++ src/main/java/com/example/caselabproject/controllers/PublicController.java
@@ -2,6 +2,7 @@ package com.example.caselabproject.controllers;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.GetMapping;
