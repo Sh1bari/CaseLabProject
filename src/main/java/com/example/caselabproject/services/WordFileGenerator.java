4865d10a71a4900ef3eb30e0e3d138642475951e
Oleg Minenko <justprog2020@yandex.ru>
2023-10-29 13:58:11 +0300

    Реализован функционал для генерации word файла для Document по documentId и отдача пользователю в виде Resource

--- src/main/java/com/example/caselabproject/services/WordFileGenerator.java
+++ src/main/java/com/example/caselabproject/services/WordFileGenerator.java
@@ -3,5 +3,5 @@ package com.example.caselabproject.services;
 import javax.validation.constraints.Min;
 
 public interface WordFileGenerator {
-    String generateWordFileForDocumentById(@Min(1L) Long documentId);
+    byte[] generateWordFileForDocumentById(@Min(1L) Long documentId);
 }
--- src/main/java/com/example/caselabproject/services/implementations/WordFileGeneratorImpl.java
+++ src/main/java/com/example/caselabproject/services/implementations/WordFileGeneratorImpl.java
@@ -2,39 +2,37 @@ package com.example.caselabproject.services.implementations;
 
 import com.example.caselabproject.exceptions.DocumentDoesNotExistException;
 import com.example.caselabproject.exceptions.WordFileCreationException;
-import com.example.caselabproject.models.entities.*;
 import com.example.caselabproject.models.entities.Document;
+import com.example.caselabproject.models.entities.DocumentConstructorType;
+import com.example.caselabproject.models.entities.Field;
+import com.example.caselabproject.models.entities.PersonalUserInfo;
 import com.example.caselabproject.repositories.DocumentRepository;
 import com.example.caselabproject.services.WordFileGenerator;
 import lombok.RequiredArgsConstructor;
 import org.apache.poi.xwpf.usermodel.*;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTPageMar;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
-import java.io.FileOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 @Validated
 public class WordFileGeneratorImpl implements WordFileGenerator {
     private final DocumentRepository documentRepository;
-    @Value("${file.temp-dir}")
-    private String tempDirectory;
 
     /**
      * Создает word файл для документа.
      *
      * @param documentId идентификатор документа, для которого необходимо сгенерировать word файл.
-     * @return path, по которому расположен созданный документ.
+     * @return документ в виде массива байт.
      */
     @Override
-    public String generateWordFileForDocumentById(Long documentId) {
+    public byte[] generateWordFileForDocumentById(Long documentId) {
         Document document = documentRepository
                 .findById(documentId)
                 .orElseThrow(() -> new DocumentDoesNotExistException(documentId));
@@ -56,69 +54,130 @@ public class WordFileGeneratorImpl implements WordFileGenerator {
                 params, position, fullName);
     }
 
-    private String generateDocument(String title, String prefix, Long id, Map<String, String> params,
+    private byte[] generateDocument(String title, String prefix, Long id, Map<String, String> params,
                                     String creatorPosition, String creatorFullName) {
-        String path = tempDirectory + UUID.randomUUID() + ".doc";
         try (XWPFDocument document = new XWPFDocument();
-             FileOutputStream fos = new FileOutputStream(path)) {
+             ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()) {
             // Установка полей
-            CTPageMar pageMar = document.getDocument().getBody().addNewSectPr().addNewPgMar();
-            pageMar.setLeft(1440);
-            pageMar.setTop(1440);
-            pageMar.setRight(720);
-            pageMar.setBottom(1440);
+            setPageFields(document);
 
             // Создание шапки документа
-            XWPFParagraph headerParagraph = document.createParagraph();
-            headerParagraph.setAlignment(ParagraphAlignment.LEFT);
-            XWPFRun headerRun = headerParagraph.createRun();
-            headerRun.setText("Код документа: " + prefix + id);
+            createHeader(prefix, id, document);
 
             // Создание параграфа с заголовком
-            XWPFParagraph titleParagraph = document.createParagraph();
-            titleParagraph.setAlignment(ParagraphAlignment.CENTER);
-            titleParagraph.setSpacingAfter(360);
-            XWPFRun titleRun = titleParagraph.createRun();
-            titleRun.setBold(true);
-            titleRun.setText(title);
+            createTitle(title, document);
 
             // Создание параграфов с основным контентом - полями и их значениями
-            for (Map.Entry<String, String> param : params.entrySet()) {
-                // Создание параграфа для имени параметра
-                XWPFParagraph keyParagraph = document.createParagraph();
-                keyParagraph.setAlignment(ParagraphAlignment.LEFT);
-                XWPFRun keyRun = keyParagraph.createRun();
-                keyRun.setText(param.getKey() + ": ");
-
-                // Создание параграфа для значения параметра
-                XWPFParagraph valueParagraph = document.createParagraph();
-                valueParagraph.setAlignment(ParagraphAlignment.LEFT);
-                valueParagraph.setIndentationFirstLine(720);
-                XWPFRun valueRun = valueParagraph.createRun();
-                valueRun.setUnderline(UnderlinePatterns.SINGLE);
-                valueRun.setText(param.getValue());
-            }
+            createContentParagraphs(params, document);
 
             // Создание футера с полным именем и должностью создателя документа
-            XWPFParagraph footerParagraph = document.createParagraph();
-            footerParagraph.setAlignment(ParagraphAlignment.LEFT);
-            XWPFRun footerRun = footerParagraph.createRun();
-            footerRun.addBreak();
-            footerRun.setText(creatorPosition + ", " + creatorFullName);
+            createFooter(creatorPosition, creatorFullName, document);
 
             // Устанавливаем шрифт и междустрочный интервал для всех параграфов
-            for (XWPFParagraph paragraph : document.getParagraphs()) {
-                paragraph.setSpacingBetween(1.5); // Полуторный интервал
-                for (XWPFRun run : paragraph.getRuns()) {
-                    run.setFontFamily("Times New Roman");
-                    run.setFontSize(14);
-                }
-            }
+            setDocumentFont(document);
 
-            document.write(fos);
-            return path;
+            document.write(byteArrayOutputStream);
+            return byteArrayOutputStream.toByteArray();
         } catch (IOException e) {
             throw new WordFileCreationException(id);
+        }
+    }
+
+
+    /**
+     * Создает шапку документа.
+     */
+    private void createHeader(String prefix, Long id, XWPFDocument document) {
+        XWPFParagraph headerParagraph = document.createParagraph();
+        headerParagraph.setAlignment(ParagraphAlignment.LEFT);
+
+        XWPFRun headerRun = headerParagraph.createRun();
+        headerRun.setText("Код документа: " + prefix + id);
+    }
+
+    /**
+     * Устанавливает поля документа.
+     */
+    private void setPageFields(XWPFDocument document) {
+        CTPageMar pageMar = document.getDocument().getBody().addNewSectPr().addNewPgMar();
+        pageMar.setLeft(1440);
+        pageMar.setTop(1440);
+        pageMar.setRight(720);
+        pageMar.setBottom(1440);
+    }
+
+    /**
+     * Создает футер с полным именем и должностью создателя документа
+     */
+    private void createFooter(String creatorPosition, String creatorFullName, XWPFDocument document) {
+        XWPFParagraph footerParagraph = document.createParagraph();
+        footerParagraph.setAlignment(ParagraphAlignment.LEFT);
+        XWPFRun footerRun = footerParagraph.createRun();
+        footerRun.addBreak();
+        footerRun.setText(creatorPosition + ", " + creatorFullName);
+    }
+
+    /**
+     * Создает параграфы с основным контентом - полями и их значениями.
+     */
+    private void createContentParagraphs(Map<String, String> params, XWPFDocument document) {
+        for (Map.Entry<String, String> param : params.entrySet()) {
+            // Создание параграфа для имени параметра
+            createParamNameParagraph(document, param);
+
+            // Создание параграфа для значения параметра
+            createParamValueParagraph(document, param);
+        }
+    }
+
+    /**
+     * Создает параграф со значением переданного параметра.
+     */
+    private void createParamValueParagraph(XWPFDocument document, Map.Entry<String, String> param) {
+        XWPFParagraph valueParagraph = document.createParagraph();
+        valueParagraph.setAlignment(ParagraphAlignment.LEFT);
+        valueParagraph.setIndentationFirstLine(720);
+
+        XWPFRun valueRun = valueParagraph.createRun();
+        valueRun.setUnderline(UnderlinePatterns.SINGLE);
+        valueRun.setText(param.getValue());
+    }
+
+    /**
+     * Создает параграф с именем переданного параметра.
+     */
+    private void createParamNameParagraph(XWPFDocument document, Map.Entry<String, String> param) {
+        XWPFParagraph keyParagraph = document.createParagraph();
+        keyParagraph.setAlignment(ParagraphAlignment.LEFT);
+
+        XWPFRun keyRun = keyParagraph.createRun();
+        keyRun.setText(param.getKey() + ": ");
+    }
+
+    /**
+     * Создает заголовок документа.
+     */
+    private void createTitle(String title, XWPFDocument document) {
+        XWPFParagraph titleParagraph = document.createParagraph();
+        titleParagraph.setAlignment(ParagraphAlignment.CENTER);
+        titleParagraph.setSpacingAfter(360);
+
+        XWPFRun titleRun = titleParagraph.createRun();
+        titleRun.setBold(true);
+        titleRun.setText(title);
+    }
+
+
+    /**
+     * Устанавливает шрифт и междустрочный интервал для всех параграфов документа.
+     */
+    private void setDocumentFont(XWPFDocument document) {
+        for (XWPFParagraph paragraph : document.getParagraphs()) {
+            paragraph.setSpacingBetween(1.5); // Полуторный интервал
+            for (XWPFRun run : paragraph.getRuns()) {
+                run.setFontFamily("Times New Roman");
+                run.setFontSize(14);
+            }
         }
     }
 }
