ef3febcd54c60c1c7d930e3741b5518f6437f98f
Ermakov Stepan <110097131+stepansaint@users.noreply.github.com>
2023-11-02 17:37:31 +0300

    Comment DocumentConstructorTypeService

--- src/main/java/com/example/caselabproject/services/DocumentConstructorTypeService.java
+++ src/main/java/com/example/caselabproject/services/DocumentConstructorTypeService.java
@@ -16,22 +16,55 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 public interface DocumentConstructorTypeService {
+    /**
+     * Creates a new document type, that has unique identifier and name.
+     *
+     * @see com.example.caselabproject.models.entities.DocumentConstructorType
+     */
     @Transactional
     DocumentConstructorTypeCreateResponseDto create(@Valid DocumentConstructorTypeRequestDto typeRequestDto);
 
+    /**
+     * Updates an existing document type (found by <code>id</code>) by changing its name and prefix.
+     *
+     * @see com.example.caselabproject.models.entities.DocumentConstructorType
+     */
     @Transactional
     DocumentConstructorTypeUpdateResponseDto updateById(@Min(value = 1L, message = "id can't be less than 1") Long id,
                                                         @Valid DocumentConstructorTypePatchRequestDto typeRequestDto);
 
+    /**
+     * Deletes an existing document type (found by <code>id</code>)
+     * by changing its state to {@link com.example.caselabproject.models.enums.RecordState#DELETED}.
+     *
+     * @see com.example.caselabproject.models.entities.DocumentConstructorType
+     */
     @Transactional
     void deleteById(@Min(value = 1L, message = "id can't be less than 1") Long id);
 
+    /**
+     * Recovers a deleted document type (found by <code>id</code>)
+     * by changing its state to {@link com.example.caselabproject.models.enums.RecordState#ACTIVE}.
+     *
+     * @see com.example.caselabproject.models.entities.DocumentConstructorType
+     */
     @Transactional
     DocumentConstructorTypeRecoverResponseDto recoverById(@Min(value = 1L, message = "id can't be less than 1") Long id);
 
+    /**
+     * Finds an existing document type by <code>id</code>.
+     *
+     * @see com.example.caselabproject.models.entities.DocumentConstructorType
+     */
     @Transactional
     DocumentConstructorTypeByIdResponseDto getById(@Min(value = 1L, message = "id can't be less than 1") Long id);
 
+    /**
+     * Finds a {@link List} of existing document types
+     * by checking their <code>name</code> and {@link RecordState}.
+     *
+     * @see com.example.caselabproject.models.entities.DocumentConstructorType
+     */
     @Transactional
     List<DocumentConstructorTypeByIdResponseDto> getAllContaining(
             @NotBlank(message = "name must not be null and must contain at least one non-whitespace character.") String name,
