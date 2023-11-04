8337ceb369f34d3b5395fa09f02fa7a8a871e229
TheKelp1e <vova_krasnov_2004@mail.ru>
2023-11-04 23:23:45 +0300

    local commit

--- src/main/java/com/example/caselabproject/controllers/WordFileGenerationController.java
+++ src/main/java/com/example/caselabproject/controllers/WordFileGenerationController.java
@@ -9,10 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.util.InMemoryResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
