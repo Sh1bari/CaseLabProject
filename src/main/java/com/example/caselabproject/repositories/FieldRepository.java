98c082f7e82db74d522b9862fa9cc13cd79f5d1a
Oleg Minenko <justprog2020@yandex.ru>
2023-11-21 16:29:47 +0300

    Update FieldRepository and create test for it

--- src/main/java/com/example/caselabproject/repositories/FieldRepository.java
+++ src/main/java/com/example/caselabproject/repositories/FieldRepository.java
@@ -4,9 +4,9 @@ import com.example.caselabproject.models.entities.Field;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface FieldRepository extends JpaRepository<Field, Long> {
-    Optional<Field> findByNameEquals(String name);
+    List<Field> findAllByNameIn(List<String> names);
 }
