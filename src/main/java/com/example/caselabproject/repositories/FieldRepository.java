233d0632d0f22ed1cf7c859aa7de4419b4423560
Oleg Minenko <justprog2020@yandex.ru>
2023-11-21 16:12:38 +0300

    Create FieldRepository

---
+++ src/main/java/com/example/caselabproject/repositories/FieldRepository.java
@@ -0,0 +1,12 @@
+package com.example.caselabproject.repositories;
+
+import com.example.caselabproject.models.entities.Field;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface FieldRepository extends JpaRepository<Field, Long> {
+    Optional<Field> findByNameEquals(String name);
+}
