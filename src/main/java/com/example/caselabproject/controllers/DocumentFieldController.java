eb0ef8b9ef573e3b693edde882f72bbf2adb4816
Oleg Minenko <justprog2020@yandex.ru>
2023-11-23 13:47:59 +0300

    Added swagger documentation

--- src/main/java/com/example/caselabproject/controllers/DocumentFieldController.java
+++ src/main/java/com/example/caselabproject/controllers/DocumentFieldController.java
@@ -1,8 +1,14 @@
 package com.example.caselabproject.controllers;
 
+import com.example.caselabproject.exceptions.AppError;
 import com.example.caselabproject.models.DTOs.request.DocumentFieldUpdateRequestDto;
 import com.example.caselabproject.models.DTOs.response.DocumentResponseDto;
 import com.example.caselabproject.services.DocumentFieldService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
@@ -10,6 +16,7 @@ import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.UniqueElements;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,6 +32,25 @@ public class DocumentFieldController {
 
     private final DocumentFieldService documentFieldService;
 
+
+    @Operation(summary = "Replaces the field values in the document. If any of the fields did not " +
+            "exist in the document, it will be added. If any of the fields you add did not exist in " +
+            "the database, it will be created.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The document fields have been successfully updated",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = DocumentResponseDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid value of path variable or request body",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AppError.class))}),
+            @ApiResponse(responseCode = "404", description = "Document not found",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AppError.class))}),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AppError.class))}),
+    })
+    @Secured("ROLE_USER")
     @PatchMapping
     public ResponseEntity<DocumentResponseDto> replaceDocumentFields(
             @PathVariable("documentId") @Min(value = 1L, message = "Id must be >= 1") Long documentId,
