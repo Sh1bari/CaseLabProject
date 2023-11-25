d2366595b30bfcc130686e5a1e20a6bb0ad054f4
Oleg Minenko <justprog2020@yandex.ru>
2023-11-25 16:34:00 +0300

    Добавил документацию к EntityOrganizationService

--- src/main/java/com/example/caselabproject/services/EntityOrganizationService.java
+++ src/main/java/com/example/caselabproject/services/EntityOrganizationService.java
@@ -3,7 +3,17 @@ package com.example.caselabproject.services;
 import jakarta.validation.constraints.Min;
 import org.springframework.validation.annotation.Validated;
 
+/**
+ * Сервис предназначен для получения идентификатора организации, к которой прикреплена
+ * какая-либо сущность.
+ */
 @Validated
 public interface EntityOrganizationService {
+    /**
+     * Метод ищет id организации, к которой прикреплена сущность с entityId.
+     *
+     * @param entityId идентификатор сущности.
+     * @return id организации, к которой прикреплена сущность с entityId.
+     */
     Long getOrganizationIdByEntityId(@Min(value = 1L, message = "EntityId must be >= 1") Long entityId);
 }
