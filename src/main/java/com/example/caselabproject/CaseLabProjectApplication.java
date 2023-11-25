8c12408a0db78976e6c925941bb1234fa0732fc9
TheKelp1e <vova_krasnov_2004@mail.ru>
2023-11-25 17:37:42 +0300

    @PreAuth

--- src/main/java/com/example/caselabproject/CaseLabProjectApplication.java
+++ src/main/java/com/example/caselabproject/CaseLabProjectApplication.java
@@ -2,8 +2,10 @@ package com.example.caselabproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class CaseLabProjectApplication {
 
     public static void main(String[] args) {
