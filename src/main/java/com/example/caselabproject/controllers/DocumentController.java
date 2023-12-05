7dbee519ee93ab7fc03f6e777cb2749cedebca42
fvreeed <novruzovfar1d@mail.ru>
2023-12-05 16:25:45 +0300

    Create document preAuthorize

--- src/main/java/com/example/caselabproject/controllers/DocumentController.java
+++ src/main/java/com/example/caselabproject/controllers/DocumentController.java
@@ -18,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -47,6 +48,7 @@ public class DocumentController {
                                     schema = @Schema(implementation = AppError.class))
                     })
     })
+
     @PostMapping("/")
     public ResponseEntity<DocumentCreateResponseDto> createDocument(
             Principal principal,
