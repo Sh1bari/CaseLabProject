9bb4f64ad1df4b214a262a91a87ebee9e1d57466
Ermakov Stepan <110097131+stepansaint@users.noreply.github.com>
2023-12-21 19:28:57 +0300

    rearrange log-files, add them to gitignore

--- Main/src/main/java/com/example/caselabproject/scheduler/ApplicationScheduler.java
+++ Main/src/main/java/com/example/caselabproject/scheduler/ApplicationScheduler.java
@@ -1,7 +1,6 @@
 package com.example.caselabproject.scheduler;
 
 
-import com.example.caselabproject.messaging.producer.implementation.KafkaApplicationStateProducer;
 import com.example.caselabproject.models.entities.Application;
 import com.example.caselabproject.models.enums.ApplicationStatus;
 import com.example.caselabproject.models.enums.RecordState;
@@ -14,15 +13,15 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.util.List;
 
+
 @Component
 @RequiredArgsConstructor
 public class ApplicationScheduler {
+    
     private final ApplicationRepository applicationRepository;
     private final ApplicationItemServiceImpl applicationItemServiceImpl;
-
+    
     /**
-     *
-     *
      * @see Application
      */
     @Scheduled(cron = "5 * * * * *") // every 5-th second of each minute
@@ -30,15 +29,9 @@ public class ApplicationScheduler {
         List<Application> applications = applicationRepository
                 .findAllByRecordStateAndApplicationStatusAndDeadlineDateBefore(
                         RecordState.ACTIVE, ApplicationStatus.WAITING_FOR_ANSWER, LocalDateTime.now());
-
+        
         for (Application application : applications) {
             applicationItemServiceImpl.calcApplicationItemsResult(application);
         }
-    }
-    
-    private final KafkaApplicationStateProducer applicationStateProducer;
-    @Scheduled(cron = "*/1 * * * * *") // every second
-    public void test() {
-        applicationStateProducer.send(Application.builder().name("asdfadf").build());
     }
 }
